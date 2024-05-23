34c78d51d410f1f5332c160aceb144896a5884f1
Mikey Ferguson <90787330+Pirate-Hunter-Zoro@users.noreply.github.com>
2024-05-22 20:25:18 -0500

    furthered expense dialog with amount and dates

--- lib/new_expense.dart
+++ lib/new_expense.dart
@@ -10,20 +10,92 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    // When constructing a DateTime object, the only required parameter is the year
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+  }
+
+  @override
+  void dispose() {
+    // This method is called automatically when a widget is removed from the UI.
+    // ONLY STATEFUL widgets have this method!!!
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
             keyboardType: TextInputType.text,
             // Declare which keyboard you want - some are specialized for emails, etc.
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               label: Text('Title'),
             ),
           ),
+          Row(
+            children: [
+              // This row will have TWO Expanded widgets - which means each will take up 50% of the space.
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  maxLength: 50,
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  // Declare which keyboard you want - some are specialized for emails, etc.
+                  decoration: const InputDecoration(
+                    prefixText: '\$ ',
+                    label: Text('Amount'),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    const Text('Selected Date'),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  // Close out of the expense creation dialog
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Save Expense'),
+              ),
+            ],
+          )
         ],
       ),
     );
